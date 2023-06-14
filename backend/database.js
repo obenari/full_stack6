f43132a5e53c601ff630435a2d4b15b66fd60117
noadahlia <ghozlan@g.jct.ac.il>
2023-06-14 17:17:28 +0300

    Database: getTodos

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,13 @@ const connectionInfo = require("./connectionInfo").connectionInfo;
 
 var con = mysql.createConnection(connectionInfo);
 
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+
+exports.getTodos = function(userId) {
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
+}
